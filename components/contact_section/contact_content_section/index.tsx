b37794e269650ad9944a44a38f933ffe69acd748
Tamilarasu <tamilarasu@kavuthamraj-3.local>
2023-11-01 21:17:20 +0530

    favicon added

--- components/contact_section/contact_content_section/index.tsx
+++ components/contact_section/contact_content_section/index.tsx
@@ -1,12 +1,22 @@
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import Select from "@mui/material/Select";
 import TextField from "@mui/material/TextField";
 
 
 
 interface ContactContentSectionProps { }
 const ContactContentSection: React.FC<ContactContentSectionProps> = (props) => {
+  const setCategorie = [
+    "Corporate T-Shirts",
+    "School Uniforms",
+    "Sportswear",
+    "Security Uniforms",
+    "Housekeeping Uniforms",
+  ];
   const setMatchSettingsName = (value: string) => {
     console.log("value", value);
-
   };
   return (
     <div className="">
@@ -23,12 +33,43 @@ const ContactContentSection: React.FC<ContactContentSectionProps> = (props) => {
             <div className="monsterrat-bold text-sm">
               You are Looking for?
             </div>
+            <div className="py-6 md:w-1/2 md:pr-4">
+              <FormControl fullWidth>
+                <InputLabel id="role-lablel"><span className="text-black">Choose categorie*</span></InputLabel>
+                <Select
+                  labelId="role-lablel"
+                  label="Choose categorie*"
+                  sx={{
+                    '& .MuiOutlinedInput-notchedOutline': {
+                      borderColor: '#969696',
+                    },
+                    '&:hover .MuiOutlinedInput-notchedOutline': {
+                      borderColor: '#969696',
+                    },
+                    '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
+                      border: "1px solid #969696",
+                    },
+                  }}
+                >
+                  {setCategorie.map(
+                    (prop, index) => {
+                      return (
+                        <MenuItem key={index} value={prop}
+                          onClick={() => { }}>
+                          {prop}
+                        </MenuItem>
+                      );
+                    }
+                  )}
+                </Select>
+              </FormControl>
+            </div>
           </div>
           <div className="">
             <div className="monsterrat-bold text-sm">Your details</div>
             <div className="md:flex gap-8  pt-4">
               <TextField
-                label="Name*"
+                label={<span className="text-black">Name*</span>}
                 variant="outlined"
                 type="text"
                 onChange={(evt) => {
@@ -55,65 +96,7 @@ const ContactContentSection: React.FC<ContactContentSectionProps> = (props) => {
                 }}
               />
               <TextField
-                label="Last Name*"
-                variant="outlined"
-                type="text"
-                onChange={(evt) => {
-                  setMatchSettingsName(evt.target.value);
-                }}
-                sx={{
-                  backgroundColor: "white",
-                  width: "100%",
-                  marginBottom: "12px",
-
-                  "& .MuiOutlinedInput-root": {
-                    color: "black",
-                    "& fieldset": {
-                      borderColor: "#969696",
-                    },
-                    "&:hover fieldset": {
-                      borderColor: "#969696",
-                    },
-                    "&.Mui-focused fieldset": {
-                      border: "1px solid #969696",
-                    },
-                  },
-                  "& .MuiInputLabel-root": {
-                    color: "black",
-                  },
-                }}
-              />
-            </div>
-            <div className="md:flex gap-8  pt-4">
-              <TextField
-                label="E-mail*"
-                variant="outlined"
-                type="text"
-                onChange={(evt) => {
-                  setMatchSettingsName(evt.target.value);
-                }}
-                sx={{
-                  backgroundColor: "white",
-                  width: "100%",
-                  marginBottom: "12px",
-                  "& .MuiOutlinedInput-root": {
-                    "& fieldset": {
-                      borderColor: "#969696",
-                    },
-                    "&:hover fieldset": {
-                      borderColor: "#969696",
-                    },
-                    "&.Mui-focused fieldset": {
-                      border: "1px solid #969696",
-                    },
-                  },
-                  "& .MuiInputLabel-root": {
-                    color: "black",
-                  },
-                }}
-              />
-              <TextField
-                label="Phone no*"
+                label={<span className="text-black">Last Name*</span>}
                 variant="outlined"
                 type="text"
                 onChange={(evt) => {
@@ -144,7 +127,7 @@ const ContactContentSection: React.FC<ContactContentSectionProps> = (props) => {
             </div>
             <div className="md:flex gap-8  pt-4">
               <TextField
-                label="City*"
+                label={<span className="text-black">E-mail*</span>}
                 variant="outlined"
                 type="text"
                 onChange={(evt) => {
@@ -171,7 +154,65 @@ const ContactContentSection: React.FC<ContactContentSectionProps> = (props) => {
                 }}
               />
               <TextField
-                label="Country*"
+                label={<span className="text-black">Phone no*</span>}
+                variant="outlined"
+                type="text"
+                onChange={(evt) => {
+                  setMatchSettingsName(evt.target.value);
+                }}
+                sx={{
+                  backgroundColor: "white",
+                  width: "100%",
+                  marginBottom: "12px",
+
+                  "& .MuiOutlinedInput-root": {
+                    color: "black",
+                    "& fieldset": {
+                      borderColor: "#969696",
+                    },
+                    "&:hover fieldset": {
+                      borderColor: "#969696",
+                    },
+                    "&.Mui-focused fieldset": {
+                      border: "1px solid #969696",
+                    },
+                  },
+                  "& .MuiInputLabel-root": {
+                    color: "black",
+                  },
+                }}
+              />
+            </div>
+            <div className="md:flex gap-8  pt-4">
+              <TextField
+                label={<span className="text-black">City*</span>}
+                variant="outlined"
+                type="text"
+                onChange={(evt) => {
+                  setMatchSettingsName(evt.target.value);
+                }}
+                sx={{
+                  backgroundColor: "white",
+                  width: "100%",
+                  marginBottom: "12px",
+                  "& .MuiOutlinedInput-root": {
+                    "& fieldset": {
+                      borderColor: "#969696",
+                    },
+                    "&:hover fieldset": {
+                      borderColor: "#969696",
+                    },
+                    "&.Mui-focused fieldset": {
+                      border: "1px solid #969696",
+                    },
+                  },
+                  "& .MuiInputLabel-root": {
+                    color: "black",
+                  },
+                }}
+              />
+              <TextField
+                label={<span className="text-black">Country*</span>}
                 variant="outlined"
                 type="text"
                 onChange={(evt) => {
@@ -202,7 +243,7 @@ const ContactContentSection: React.FC<ContactContentSectionProps> = (props) => {
             </div>
             <div className="pt-4">
               <TextField
-                label="Message*"
+                label={<span className="text-black">Message*</span>}
                 variant="outlined"
                 type="text"
                 multiline
