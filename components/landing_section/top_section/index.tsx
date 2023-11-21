0ff8fc05b0b666abe5bc41159c01922b627c00e5
Tamilarasu <tamilarasu@kavuthamraj-3.local>
2023-11-21 12:13:56 +0530

    ui chnages

--- components/landing_section/top_section/index.tsx
+++ components/landing_section/top_section/index.tsx
@@ -4,9 +4,9 @@ const TopSection: React.FC<TopSectionProps> = (props) => {
     <div className="">
       <div className="relative">
         <div className="absolute inset-0 text-white">
-          <div className="w-full md:w-1/2 lg:w-9/12 h-full px-6 lg:px-24 flex flex-col justify-center">
+          <div className="w-full md:w-1/2 lg:w-9/12 h-full px-6 lg:px-24 flex flex-col justify-center ">
             <div className="montserrat-semiBold text-5xl md:text-xl lg:text-6xl pb-2 lg:pb-3">
-              We{"&apos;"}re your one-stop
+              {`We're your one-stop`}
             </div>
             <div className="montserrat-bold text-5xl md:text-xl lg:text-6xl text-[#FFC400] pb-2 lg:pb-3">
               Manufacturer
@@ -14,33 +14,36 @@ const TopSection: React.FC<TopSectionProps> = (props) => {
             <div className="montserrat-semiBold text-5xl md:text-xl lg:text-6xl pb-6 lg:pb-10">
               For your brand.
             </div>
-            <div className="montserrat-semiBold text-base lg:text-lg pb-1">
+            <div className="montserrat-semiBold md:text-lg lg:text-2xl pb-1">
               Begin with us
             </div>
-            <div className="montserrat-medium leading-5 pr-16 lg:w-1/2" style={{fontSize: '12px'}}>
-              Share your clothing concepts with us, and we{"&apos;"}ll manage every
+            <div className="montserrat-medium leading-5 pr-16 lg:w-1/2 md:text-xs">
+              {`Share your clothing concepts with us, and we'll manage every
               aspect, from creating samples to delivering your clothing
-              production.
+              production.`}
             </div>
             <div className="flex items-start pt-8">
               <div className="mr-4 pt-2 border-t-[0.5px] border-white">
-                <div className="montserrat-bold text-3xl pb-2">
+                <div className="montserrat-bold md:text-lg lg:text-4xl pb-2">
                   200+
                 </div>
-                <div className="montserrat-semiBold lg:text-sm" style={{fontSize: '10px'}}>
+                <div className="montserrat-semiBold md:text-xs lg:text-lg">
                   satisfied clothingbrands globally
                 </div>
               </div>
               <div className="ml-4 pt-2 border-t-[0.5px] border-white">
-                <div className="montserrat-bold text-3xl pb-2">
+                <div className="montserrat-bold md:text-lg lg:text-4xl pb-2">
                   10yrs+
                 </div>
-                <div className="montserrat-semiBold lg:text-sm" style={{fontSize: '10px'}}>
+                <div className="montserrat-semiBold md:text-xs lg:text-lg">
                   Of quality industry experience
                 </div>
               </div>
             </div>
           </div>
+        </div>
+        <div className="hidden lg:block absolute right-28 bottom-28" >
+          <img src="/assets/images/png/cotton_img.png" alt="" className="w-full" />
         </div>
         <img
           src={`/assets/images/png/rang_top_bg.png`}
